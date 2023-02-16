e493c87c0b0ce9b5dc301f6493c0b849a1296cbd
platara <plat0033@algonquinlive.com>
2023-02-15 20:17:21 -0500

    add week 6

---
+++ week-6/main.js
@@ -0,0 +1,57 @@
+/*** Inserting elements with innertHTML */
+
+//Use the 'courses' array, from script.js, to create a 'HTML template' with a list of courses (display only the first two courses). Display the result in the document. Use the DOM element with the id 'list'.
+
+//Add the third course to the list
+
+//Dinamically create, and display in the document, all the ID cards. Use the 'students' array, and a 'for loop'.
+
+//Display all the IDs using the innerHTML only once. Use an array and the join() method.
+
+//Use a loop with insertAdjacentHTML to display the courses
+
+//...what if the element with the id courseList didn't exist
+
+
+
+/*** Create HMTL element with createElement() method */
+
+//1.-create a new link elment in the top menu 
+
+    // create a new element
+    
+    // add some content
+    
+    // add the newly created element to the DOM
+    
+
+
+
+
+
+//2.-Retrieve the DOM element that has the id 'student-table'
+
+//Declare a function to create a table
+
+    //using a for loop create the rows/cells, and fill them with the students name, number and email
+ 
+        // Assign an array's element to a variable 
+ 
+        // Create a new 'table row' for the contact 
+
+            // Create a new cell inside of the row
+ 
+            // fill the value of the cell 
+
+            //insert the cell in the row element
+
+            //Repeat 'create...', 'fill...' and 'insert...'
+        
+        //insert the row in the table element 
+        
+    // end for
+// end function
+
+
+//Call the function to display the table in the page
+
