4b80801ec5d5da8c5946528f9c2e47e4e5e59145
platara <plat0033@algonquinlive.com>
2023-02-22 09:51:18 -0500

    week-7

---
+++ week-7/main.js
@@ -0,0 +1,50 @@
+//Create your HTML, Javascript file
+//Install bootstrap
+//Ceate an array of objects(data structure) to manage store items; use two properties(product, price) in the object; create two products.
+
+// print the first product in the console; retrieve the 'product' property.
+
+// add one extra item to the 'products' array
+
+
+//update the price of the 'Hoodie' product to 50.00
+
+
+//add the property 'size'; use the 'Tablet' product
+
+
+//create an array of strings; on every element of the array describe one product
+
+//add the property 'description' to all the products(objects); use the 'description' array
+
+//Delete the property 'size'; use the 'Tablet' product
+
+
+//go back to the object definition and create a method(function), as a parameter, to update the 'price' of a product. Test it in the console.
+
+//Do the same with a regular function
+    //1.declare the function; identify the inputs that you need.
+
+    //2.find the right product
+
+    //3.assign the new value
+
+
+//display all the products in the 'document'; you can use a bootstrap card, for...of loop, and innnerHTML. 
+
+    //1.Define varibles; 'DOM element' and a empty array
+
+    //2.define your 'html template'; push 'html code'(template) to the array.
+
+    //3.join and insert the complete 'html template'
+
+
+
+
+/*** JavaScript Object methods */
+//instead of the cards, display a table with all the products; Reuse the '$body' and 'htmlTemplate'; use the Object methods.
+
+    //1.Clear htmlTemplate
+
+    //2.repeat steps 2 and 3 from previous example
+
