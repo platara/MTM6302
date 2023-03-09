dafd6cfb020bde52c34b472f76e1b23c1f6492d6
davidplatar <david.plata@hotmail.com>
2023-03-09 14:00:27 -0500

    add form

--- week-9/main.js
+++ week-9/main.js
@@ -11,7 +11,28 @@ $edithStudentBtn.addEventListener('mouseover', function () {
 
   })
 
-// 3.Insert a HTML form in the detail info.
+// 3.Insert a 'HTML form' in the detail info.
+const $modalBody = document.getElementById('modal-body')
+
+$modalBody.innerHTML = `
+            <form>
+                <div class="mb-3">
+                  <label for="exampleInputEmail1" class="form-label">Email address</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="${students[0].email}">
+                  <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                  <label for="exampleInputPassword1" class="form-label">User name</label>
+                  <input type="text" class="form-control" id="exampleInputPassword1" value="${students[0]['userName']}">
+                </div>
+                <div class="mb-3 form-check">
+                  <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                  <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+`
+
 
 // 4.Add an event listener to the 'form'. Update the student info.
 
