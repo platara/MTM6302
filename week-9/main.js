7f642e23f7d6f6b79e589aad0cc7b79dcb7782df
platara <plat0033@algonquinlive.com>
2023-03-08 20:44:30 -0500

    add week-9

---
+++ week-9/main.js
@@ -0,0 +1,21 @@
+// 1.Create a button in the student card(html file), and retrive it(js file).
+
+// 2.Add an event listener to the button. Show the detail info.
+
+// 3.Insert a HTML form in the detail info.
+
+// 4.Add an event listener to the 'form'. Update the student info.
+
+// 5. Create a 'search' box. in the html file.
+
+// 6. Add an event listener to the 'Search' form. Use the 'submit' event. Display a filtered list based on the student name.
+
+// 7.Add an event listener to the 'Search' element. Use the 'input' event. Display a filtered list based on the student name.
+
+
+
+
+
+
+
+
