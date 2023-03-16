f5d00f119845b8565a3297797e2e9c9c7de6ebf3
davidplatar <david.plata@hotmail.com>
2023-03-16 10:14:40 -0400

    add event delegation

--- week-10/main.js
+++ week-10/main.js
@@ -81,15 +81,52 @@ console.log(  $studentNumber.dataset.studentNumber )
 
 const $buttons = document.querySelectorAll('.ac-card-info button')
 
+// 9. Add an event listener in all the 'ac-card' buttons. 
 for (const button of $buttons) {
-  button.addEventListener('click', function(){
-    console.log('click')
+  button.addEventListener('click', function(event){
+    console.log(event.target.dataset.studentIndex)
+    event.target
+    $modalBody.innerHTML = `
+            <form id="form">
+                <div class="mb-3">
+                  <label for="exampleInputEmail1" class="form-label">Email address</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="${students[event.target.dataset.studentIndex].email}" name="email">
+                  <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                  <label for="exampleInputPassword1" class="form-label">User name</label>
+                  <input type="text" class="form-control" id="exampleInputPassword1" value="${students[event.target.dataset.studentIndex]['userName']}" name="userName">
+                </div>
+
+                <div class="mb-3">
+                  <label for="studentNumber" class="form-label">Student number</label>
+                  <input type="text" class="form-control" id="studentNumber" value="${students[event.target.dataset.studentIndex].studentNumber}" name="userName">
+                </div>
+
+                <div class="mb-3 form-check">
+                  <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                  <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+`
    })
   }
 
-// 9. Add an event listener in all the 'ac-card' buttons. 
+
 
 // 10 Use the event delegation to listen to the 'submit' event in the student details view (modal).
+
+document.getElementById('studentID0').addEventListener('click', function(e){
+  
+  if (e.target.classList.contains('btn')) {
+    console.log('click')
+  }
+})
+
+
+
+
 
 
 
